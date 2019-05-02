47ed965d6d201c0fcf20854f9510c4e05824d251
anton-voron <49334029+anton-voron@users.noreply.github.com>
2019-05-02 23:38:15 +0300

    js for modal + slider

--- js/index.js
+++ js/index.js
@@ -1,1 +1,63 @@
-﻿
+﻿var contactUs = document.querySelector(".button-contact");
+var popup = document.querySelector(".contuct-us");
+var close = popup.querySelector(".modal-close");
+var popupName = popup.querySelector("[name=user-name]");
+var popupEmail = popup.querySelector("[name=user-emai]");
+var form = popup.querySelector(".contact-details");
+var isStorageSupport = true;
+var storageName = "";
+var storageEmail = "";
+
+try{
+	storageName= localStorage.getItem("popupName");
+	storageEmail= localStorage.getItem("popupEmail");
+} catch (err) {
+	isStorageSupport = false;
+}
+
+contactUs.addEventListener("click", function(evt){
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+	if(storageName){
+		popupName.value = storageName;
+		popupEmail.focus();
+	} else {
+		popupName.focus();
+	}
+});
+form.addEventListener("submit", function(evt){
+	if(!popupName.value || !popupName.value){
+		evt.preventDefault();
+		popup.classList.add("modal-error");
+	} else {
+		if(isStorageSupport){
+			localStorage.setImet("popupName", popupName.value);
+			localStorage.setImet("popupEmail", popupEmail.value);
+		}
+	}
+});
+close.addEventListener("click", function(evt){
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error"); /*АНТОН, НАПИШИ АНИМАЦИЮ!!!*/
+});
+window.addEventListener("keydown", function(evt){
+	if(popup.classList.contains("modal-show")){
+		if(evt.keyCode==27){
+			evt.preventDefault();
+			popup.classList.remove("modal-show");
+		}
+	}
+});
+
+/*   SLIDER */
+
+var slides = document.querySelectorAll('.slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 3000);
+ 
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
